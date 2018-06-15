3454207259079681b64c1c42c4c90b312be3202d
zldang <38782010+zldang@users.noreply.github.com>
2018-06-15 12:52:58 +0700

    Update cloudflare-certbot.sh

--- inc/cloudflare/cloudflare-certbot.sh
+++ inc/cloudflare/cloudflare-certbot.sh
@@ -1,6 +1,22 @@
 #!/bin/bash
+
+# Ubuntu
+sudo apt-get update
+sudo apt-get install software-properties-common
+sudo add-apt-repository ppa:certbot/certbot
+sudo apt-get update
+sudo apt-get install python-certbot-nginx 
+sudo apt-get install python3-pip python3-dev
+sudo pip3 install pyasn1 setuptools wheel pyyaml
+sudo pip3 install certbot-dns-cloudflare
+
+domain_name=''
+
 sudo certbot certonly \
   --dns-cloudflare \
   --dns-cloudflare-credentials /home/$user/cloudflare.ini \
-  -d domain_name \
-  -d www.domain_name
+  --register-unsafely-without-email \
+  -d $domain_name \
+  -d www.$domain_name
+  
+sudo openssl dhparam -out /etc/nginx/ssl/dhparam.pem 2048
